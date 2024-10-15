b0b85cca697ead68d4d3c1514a3de83ca1082f33
PSBLaLaHey <pizz12547@gmail.com>
2024-10-15 10:48:22 +0700

    r

--- app/page.js
+++ app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
               src="https://nextjs.org/icons/vercel.svg"
               alt="Vercel logomark"
               width={200}
-              height={20}
+              height={200}
             />
             Deploy now
           </a>
