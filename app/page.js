a87e9e12909e03ca276cb6eb78f2e1ff31d6d28f
PSBLaLaHey <pizz12547@gmail.com>
2024-10-16 09:11:50 +0700

    a

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
           width={180}
-          height={38}
+          height={39}
           priority
         />
         <ol>
